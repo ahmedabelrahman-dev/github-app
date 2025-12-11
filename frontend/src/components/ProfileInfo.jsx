d940162796cce19a528605bf234c69f643c665c8
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-11 19:13:53 +0400

    fix: profile info component

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -8,24 +8,9 @@ import { FaXTwitter } from 'react-icons/fa6';
 import { TfiThought } from 'react-icons/tfi';
 import { FaEye } from 'react-icons/fa';
 import { formatMemberSince } from '../utils/functions';
-// import LikeProfile from './LikeProfile';
+import LikeProfile from './LikeProfile';
 
 const ProfileInfo = ({ userProfile }) => {
-  // const userProfile = {
-  // 	avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-  // 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-  // 	email: "johndoe@gmail.com",
-  // 	followers: 100,
-  // 	following: 200,
-  // 	html_url: "https://github.com/burakorkmez",
-  // 	location: "Somewhere, Earth",
-  // 	name: "John Doe",
-  // 	public_gists: 100,
-  // 	public_repos: 100,
-  // 	twitter_username: "johndoe",
-  // 	login: "johndoe",
-  // };
-
   const memberSince = formatMemberSince(userProfile?.created_at);
 
   return (
@@ -43,7 +28,7 @@ const ProfileInfo = ({ userProfile }) => {
           {/* View on Github */}
 
           <div className="flex gap-2 items-center flex-col">
-            {/* <LikeProfile userProfile={userProfile} /> */}
+            <LikeProfile userProfile={userProfile} />
             <a
               href={userProfile?.html_url}
               target="_blank"
@@ -143,3 +128,18 @@ const ProfileInfo = ({ userProfile }) => {
   );
 };
 export default ProfileInfo;
+
+// const userProfile = {
+// 	avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+// 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
+// 	email: "johndoe@gmail.com",
+// 	followers: 100,
+// 	following: 200,
+// 	html_url: "https://github.com/burakorkmez",
+// 	location: "Somewhere, Earth",
+// 	name: "John Doe",
+// 	public_gists: 100,
+// 	public_repos: 100,
+// 	twitter_username: "johndoe",
+// 	login: "johndoe",
+// };
